cd7ad65efc5ebc3041822a8daee29472d78dbf2b
Salooma <saloomaali2000@gmail.com>
2022-11-21 23:30:18 +0530

    student class created

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 
 class Student{
     String name;
+    int rollNo;
+    String admsnNO;
+    String college;
 
-
+    public Student(String name, int rollNo, String admsnNO, String college) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.admsnNO = admsnNO;
+        this.college = college;
+    }
 }
 public class Main {
     public static void main(String[] args) {
